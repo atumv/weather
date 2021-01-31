def76e6109d743889f1f05f2560ab54522c52c49
atumv <atumv.gm@gmail.com>
2021-02-01 00:53:05 +0700

    Initial commit

--- src/Router.js
+++ src/Router.js
@@ -7,7 +7,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={App} exact />
+        <Route path="/image-search" component={App} exact />
         <Route path="/image/:id" component={ImageView} />
       </Switch>
     </BrowserRouter>
