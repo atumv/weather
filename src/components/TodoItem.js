3cbcff8f27727f70ccab9d96b29c673e6cc6903a
atumv <atumv.gm@gmail.com>
2021-04-23 13:11:11 +0700

    Initial commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,25 +1,34 @@
 import React from "react";
+import { toggleTodo, removeTodo } from "../redux/actions";
+import { useDispatch } from "react-redux";
 
-const TodoItem = ({ todo, index, toggleTodo, removeTodo }) => (
-  <label>
-    <li className="todo-item">
-      <div className="input-wrap">
-        <input
-          className="check-custom"
-          type="checkbox"
-          checked={todo.completed}
-          onChange={() => toggleTodo(todo.id)}
-        />
-        <span className="check-toggle" />
-        <span className="task-number">{index + 1}</span>
-        <span className="task-title">{todo.title}</span>
-      </div>
+const TodoItem = ({ todo, index }) => {
+  const dispatch = useDispatch();
 
-      <button className="rm-btn" onClick={() => removeTodo(todo.id)}>
-        &times;
-      </button>
-    </li>
-  </label>
-);
+  return (
+    <label>
+      <li className="todo-item">
+        <div className="input-wrap">
+          <input
+            className="check-custom"
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => dispatch(toggleTodo(todo.id))}
+          />
+          <span className="check-toggle" />
+          <span className="task-number">{index + 1}</span>
+          <span className="task-title">{todo.title}</span>
+        </div>
+
+        <button
+          className="rm-btn"
+          onClick={() => dispatch(removeTodo(todo.id))}
+        >
+          &times;
+        </button>
+      </li>
+    </label>
+  );
+};
 
 export default TodoItem;
